2a5419ac23bbb82f731d27b7528b533490d0f29f
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-04-13 19:32:37 +0300

    date done

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +56,15 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   if (date.getFullYear() % 4 == 0) {
+      if (date.getFullYear() % 400 == 0) {
+         return true
+      }
+      if (date.getFullYear() % 100 == 0) {
+         return false
+      }
+      return true
+   } else return false
 }
 
 
@@ -76,7 +84,20 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let duration = endDate - startDate;
+   var milliseconds = parseInt((duration % 1000)),
+      seconds = Math.floor((duration / 1000) % 60),
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+   milliseconds = (milliseconds < 100) ? "00" + milliseconds : milliseconds;
+   hours = (hours < 10) ? "0" + hours : hours;
+   minutes = (minutes < 10) ? "0" + minutes : minutes;
+   seconds = (seconds < 10) ? "0" + seconds : seconds;
+   let str = hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+   if (milliseconds.toString() == '0') {
+      return hours + ":" + minutes + ":" + seconds + "." + '000'
+   }
+   else { return str };
 }
 
 
@@ -94,14 +115,25 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
-}
+   let res = 0.5 * (60 * date.getUTCHours() + date.getUTCMinutes()) - 6 * date.getUTCMinutes();
+   if (res < 180) {
+     return res * Math.PI / 180
+   } else if (res < 360) {
+     res = 360 - res;
+     return Math.abs(res) * Math.PI / 180
+   } else if (res > 360) {
+     while (res > 180) {
+       res = res - 180
+     }
+     return res * Math.PI / 180
+   }
+ }
 
 
 module.exports = {
-    parseDataFromRfc2822: parseDataFromRfc2822,
-    parseDataFromIso8601: parseDataFromIso8601,
-    isLeapYear: isLeapYear,
-    timeSpanToString: timeSpanToString,
-    angleBetweenClockHands: angleBetweenClockHands
+   parseDataFromRfc2822: parseDataFromRfc2822,
+   parseDataFromIso8601: parseDataFromIso8601,
+   isLeapYear: isLeapYear,
+   timeSpanToString: timeSpanToString,
+   angleBetweenClockHands: angleBetweenClockHands
 };
